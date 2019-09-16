06c8272a1895bd83514260386bc6550cc73c0ed0
Tate Stone <tatejamesstone@gmail.com>
2019-09-16 09:51:54 -0600

    First Commit

--- src/store.js
+++ src/store.js
@@ -1,1 +1,24 @@
+import {createStore} from 'redux'
 
+const initialState = {
+    recipeName: '',
+    recipeCategory: ''
+}
+
+export const UPDATE_RECIPE_NAME = 'UPDATE_RECIPE_NAME'
+
+export const UPDATE_RECIPE_CATEGORY = 'UPDATE_RECIPE_CATEGORY'
+
+function reducer(state = initialState, action) {
+    const {type, payload} = action
+    switch (type) {
+        case UPDATE_RECIPE_NAME:
+            return {...state, name:payload}
+        case UPDATE_RECIPE_CATEGORY:
+            return {...state, category: payload}
+        default:
+            return state
+    }
+}
+
+export default createStore(reducer)
